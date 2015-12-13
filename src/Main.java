ed8cdbecd41b6278a67e276a380fea338e2ca733
JWahlers <JWahlers15@gmail.com>
2015-12-13 16:49:03 -0500

    Create Main.java

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+
+public class Main 
+{
+    //Regex definitions
+    // static final String STATE =  "[(]?(\\d+, )*(\\d+)+[)]?";
+
+    public static void main(String[] args)
+    {
+        PNCGUI pnc = new PNCGUI();
+        pnc.setVisible(true);
+    }
+
+}
